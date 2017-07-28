cbd16342b98d93589f91c51b7ad0257712b631a3
György Mátyás <gy.matyas97@gmail.com>
2017-07-28 16:07:43 +0200

    added description comments

--- bowling.py
+++ bowling.py
@@ -1,4 +1,9 @@
 def score(game):
+    """
+    # Description: Calculates the total score of the game.
+    # Inputs: game - The log of the game.
+    # Returns: The game's total score.
+    """
     result = 0
     frame = 1
     in_first_half = True
@@ -13,6 +18,14 @@ def score(game):
 
 
 def game_logic(current_roll, game, frame, result):
+    """
+    # Description : Adds the current roll's score to total result
+    # Inputs: current_roll - The player's current roll
+    #         game - The player's every roll
+    #         frame - The number of the current frame (round)
+    #         result - The total result
+    # Returns: The result after adding the current roll
+    """
     if game[current_roll] == '/' and frame < 10:
         result += 10 - get_value(game[current_roll - 1]) + get_value(game[current_roll + 1])
     elif game[current_roll].upper() == 'X' and frame < 10:
@@ -27,4 +40,9 @@ def game_logic(current_roll, game, frame, result):
 
 
 def get_value(roll):
+    """
+    # Description: Returns the current roll's value
+    # Inputs: roll - The player's roll
+    # Returns: The value of the roll in number
+    """
     return int(roll) if roll.isdigit() else 10 if roll.upper() in 'X/' else 0 if roll == '-' else ValueError
