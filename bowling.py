8534b34751dc7e3eedda974dfe0f0fb74b75b60f
György Mátyás <gy.matyas97@gmail.com>
2017-07-28 15:43:30 +0200

    simplyfied logic in score()

--- bowling.py
+++ bowling.py
@@ -3,19 +3,14 @@ def score(game):
     frame = 1
     in_first_half = True
     for current_roll in range(len(game)):
-        if game[current_roll] == '/':
-            result += 10 - get_value(game[current_roll - 1])
-        else:
-            result += get_value(game[current_roll])
-        if frame < 10  and get_value(game[current_roll]) == 10:
-            if game[current_roll] == '/':
-                result += get_value(game[current_roll+1])
-            elif game[current_roll] == 'X' or game[current_roll] == 'x':
-                result += get_value(game[current_roll+1])
-                if game[current_roll+2] == '/':
-                    result += 10 - get_value(game[current_roll+1])
-                else:
-                    result += get_value(game[current_roll+2])
+        if game[current_roll] == '/' and frame < 10:
+            result += 10 - get_value(game[current_roll - 1]) + get_value(game[current_roll + 1])
+        elif game[current_roll].upper() == 'X' and frame < 10:
+            result += get_value(game[current_roll]) + get_value(game[current_roll + 1])
+            if game[current_roll + 2] == '/':
+                result += 10 - get_value(game[current_roll + 1])
+            else:
+                result += get_value(game[current_roll + 2])
         if in_first_half and not game[current_roll].upper() == 'X':
             in_first_half = False
         else:
