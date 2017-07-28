90cddae11bad5c8caaf2d042498e0fa1f4fbc6fe
György Mátyás <gy.matyas97@gmail.com>
2017-07-28 16:06:29 +0200

    defined game_logic()

--- bowling.py
+++ bowling.py
@@ -3,16 +3,7 @@ def score(game):
     frame = 1
     in_first_half = True
     for current_roll in range(len(game)):
-        if game[current_roll] == '/' and frame < 10:
-            result += 10 - get_value(game[current_roll - 1]) + get_value(game[current_roll + 1])
-        elif game[current_roll].upper() == 'X' and frame < 10:
-            result += get_value(game[current_roll]) + get_value(game[current_roll + 1])
-            if game[current_roll + 2] == '/':
-                result += 10 - get_value(game[current_roll + 1])
-            else:
-                result += get_value(game[current_roll + 2])
-        else:
-            result += get_value(game[current_roll])
+        result = game_logic(current_roll, game, frame, result)
         if in_first_half and not game[current_roll].upper() == 'X':
             in_first_half = False
         else:
@@ -20,10 +11,20 @@ def score(game):
             frame += 1
     return result
 
+
+def game_logic(current_roll, game, frame, result):
+    if game[current_roll] == '/' and frame < 10:
+        result += 10 - get_value(game[current_roll - 1]) + get_value(game[current_roll + 1])
+    elif game[current_roll].upper() == 'X' and frame < 10:
+        result += get_value(game[current_roll]) + get_value(game[current_roll + 1])
+        if game[current_roll + 2] == '/':
+            result += 10 - get_value(game[current_roll + 1])
+        else:
+            result += get_value(game[current_roll + 2])
+    else:
+        result += get_value(game[current_roll])
+    return result
+
+
 def get_value(roll):
-    #######
-    # Description: Returns the current roll's score
-    # Inputs: roll - player's current roll
-    # Returns: the value of the roll in number
-    #######
     return int(roll) if roll.isdigit() else 10 if roll.upper() in 'X/' else 0 if roll == '-' else ValueError
