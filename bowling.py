5b13e4f9027f6435b604fc8031203aeec037df15
György Mátyás <gy.matyas97@gmail.com>
2017-07-28 15:41:24 +0200

    renamed magic variable to a more describing name

--- bowling.py
+++ bowling.py
@@ -2,21 +2,21 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - get_value(game[i - 1])
+    for current_roll in range(len(game)):
+        if game[current_roll] == '/':
+            result += 10 - get_value(game[current_roll - 1])
         else:
-            result += get_value(game[i])
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+            result += get_value(game[current_roll])
+        if frame < 10  and get_value(game[current_roll]) == 10:
+            if game[current_roll] == '/':
+                result += get_value(game[current_roll+1])
+            elif game[current_roll] == 'X' or game[current_roll] == 'x':
+                result += get_value(game[current_roll+1])
+                if game[current_roll+2] == '/':
+                    result += 10 - get_value(game[current_roll+1])
                 else:
-                    result += get_value(game[i+2])
-        if in_first_half and not game[i].upper() == 'X':
+                    result += get_value(game[current_roll+2])
+        if in_first_half and not game[current_roll].upper() == 'X':
             in_first_half = False
         else:
             in_first_half = True
